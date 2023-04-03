b20c1deafce7ccef9bc7c57faca20bc10398d274
Timo <timo.melnicki@hs-furtwangen.de>
2023-04-03 11:00:49 +0200

    gravupdate

--- SonicSpiel/Script/Build/Script.js
+++ SonicSpiel/Script/Build/Script.js
@@ -4,36 +4,36 @@ var Script;
     var ƒ = FudgeCore;
     ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
     class CustomComponentScript extends ƒ.ComponentScript {
-        // Register the script as component for use in the editor via drag&drop
-        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
-        // Properties may be mutated by users in the editor via the automatically created user interface
-        message = "CustomComponentScript added to ";
         constructor() {
             super();
+            // Properties may be mutated by users in the editor via the automatically created user interface
+            this.message = "CustomComponentScript added to ";
+            // Activate the functions of this component as response to events
+            this.hndEvent = (_event) => {
+                switch (_event.type) {
+                    case "componentAdd" /* COMPONENT_ADD */:
+                        ƒ.Debug.log(this.message, this.node);
+                        break;
+                    case "componentRemove" /* COMPONENT_REMOVE */:
+                        this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                        this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                        break;
+                    case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                        // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                        break;
+                }
+            };
             // Don't start when running in editor
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
             // Listen to this component being added to or removed from a node
-            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
-            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
-            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
         }
-        // Activate the functions of this component as response to events
-        hndEvent = (_event) => {
-            switch (_event.type) {
-                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
-                    ƒ.Debug.log(this.message, this.node);
-                    break;
-                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
-                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
-                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
-                    break;
-                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
-                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
-                    break;
-            }
-        };
     }
+    // Register the script as component for use in the editor via drag&drop
+    CustomComponentScript.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
     Script.CustomComponentScript = CustomComponentScript;
 })(Script || (Script = {}));
 var Script;
@@ -44,6 +44,9 @@ var Script;
     let viewport;
     let graph;
     let knuckles;
+    let gravity = -9.81;
+    let ySpeed = 0;
+    let isGrounded = true;
     //let isGrounded: boolean = true;
     //let gravity: number = 0.3;
     //let ySpeed: number = 1;
@@ -55,33 +58,47 @@ var Script;
         knuckles.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4));
         let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
         viewport.camera = cmpCamera;
-        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
-        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); //(ƒ.LOOP_MODE.TIME_GAME, 30);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
         //ySpeed += gravity;
         viewport.draw();
         ƒ.AudioManager.default.update();
+        let timeFrame = ƒ.Loop.timeFrameGame / 1000; // time since last frame in seconds
         //let graph: ƒ.Node = viewport.getBranch();
         //let knuckles: ƒ.Node = graph.getChildrenByName("Charakter")[0].getChildrenByName("knuckles")[0];
         //console.log(graph.getChildrenByName("Charakter")[0].getChildrenByName("knuckles")[0]);
         //knuckles.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4));
         //knuckles.mtxLocal.translateX(1);+
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
-            knuckles.mtxLocal.translateX(0.1);
+            knuckles.mtxLocal.translateX(2 * timeFrame);
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
-            knuckles.mtxLocal.translateX(-0.1);
-        let pos = knuckles.mtxLocal.translation;
+            knuckles.mtxLocal.translateX(-2 * timeFrame);
         //if (pos.y > -0.5)
         //knuckles.mtxLocal.translateY(-0.2);
         //else
         //knuckles.mtxLocal.translateY(+1);
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]))
-            knuckles.mtxLocal.translateY(+1);
+        //if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]))
+        //knuckles.mtxLocal.translateY(+1);
         //isGrounded = false;
-        if (pos.y > 0)
-            knuckles.mtxLocal.translateY(-0.3);
+        //if (pos.y > 0)
+        //knuckles.mtxLocal.translateY(-0.3);
+        if (isGrounded && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
+            ySpeed = 5;
+            isGrounded = false;
+        }
+        ySpeed += gravity * timeFrame;
+        let pos = knuckles.mtxLocal.translation;
+        pos.y += ySpeed * timeFrame;
+        if (pos.y < 0) {
+            ySpeed = 0;
+            pos.y = 0;
+            isGrounded = true;
+        }
+        knuckles.mtxLocal.translation = pos;
+        viewport.draw();
     }
 })(Script || (Script = {}));
 //mtxLocal.translation.y = 0 matrix translation an Y
--- SonicSpiel/Script/Source/Main.ts
+++ SonicSpiel/Script/Source/Main.ts
@@ -6,6 +6,9 @@ namespace Script {
   let viewport: ƒ.Viewport;
   let graph: ƒ.Node;
   let knuckles: ƒ.Node; 
+  let gravity: number = -9.81;
+  let ySpeed: number = 0;
+  let isGrounded: boolean = true;
   //let isGrounded: boolean = true;
   //let gravity: number = 0.3;
   //let ySpeed: number = 1;
@@ -19,7 +22,7 @@ namespace Script {
     let cmpCamera: ƒ.ComponentCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
     viewport.camera = cmpCamera;
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start(); //(ƒ.LOOP_MODE.TIME_GAME, 30);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
@@ -27,27 +30,41 @@ namespace Script {
     //ySpeed += gravity;
     viewport.draw();
     ƒ.AudioManager.default.update();
+    let timeFrame: number = ƒ.Loop.timeFrameGame / 1000; // time since last frame in seconds
     //let graph: ƒ.Node = viewport.getBranch();
     //let knuckles: ƒ.Node = graph.getChildrenByName("Charakter")[0].getChildrenByName("knuckles")[0];
     //console.log(graph.getChildrenByName("Charakter")[0].getChildrenByName("knuckles")[0]);
     //knuckles.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4));
     //knuckles.mtxLocal.translateX(1);+
     if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
-      knuckles.mtxLocal.translateX(0.1);
+      knuckles.mtxLocal.translateX(2 * timeFrame);
 
     if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
-      knuckles.mtxLocal.translateX(-0.1);
+      knuckles.mtxLocal.translateX(-2 * timeFrame);
 
-    let pos: ƒ.Vector3 = knuckles.mtxLocal.translation;
     //if (pos.y > -0.5)
       //knuckles.mtxLocal.translateY(-0.2);
     //else
       //knuckles.mtxLocal.translateY(+1);
-      if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]))
-        knuckles.mtxLocal.translateY(+1);
+      //if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]))
+        //knuckles.mtxLocal.translateY(+1);
         //isGrounded = false;
-      if (pos.y > 0)
-        knuckles.mtxLocal.translateY(-0.3);
+      //if (pos.y > 0)
+        //knuckles.mtxLocal.translateY(-0.3);
+      if (isGrounded && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
+        ySpeed = 5;
+        isGrounded = false;
+  }
+  ySpeed += gravity * timeFrame;
+    let pos: ƒ.Vector3 = knuckles.mtxLocal.translation;
+    pos.y += ySpeed * timeFrame;
+    if (pos.y < 0) {
+      ySpeed = 0;
+      pos.y = 0;
+      isGrounded = true;
+    }
+    knuckles.mtxLocal.translation = pos;
+    viewport.draw();
   }
 }
 
