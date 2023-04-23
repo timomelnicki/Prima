e2e8fbe2f4cfe4054eb03a0aec7b83c9b94c36c4
Timo <timo.melnicki@hs-furtwangen.de>
2023-04-23 15:43:46 +0200

    cubes

--- Minecraft/Script/Source/Main.ts
+++ Minecraft/Script/Source/Main.ts
@@ -5,9 +5,12 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let cubeBlue: ƒ.Graph;
+  let world: ƒ.Graph;
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
+    generateWorld();
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
@@ -17,4 +20,28 @@ namespace Script {
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+
+  function generateWorld(){
+    let displacement: number = 1.01;
+    let index: number = 0;
+    let size: number = 5;
+    let cubes: ƒ.Node[] = new Array (size * size * size);
+
+    let transform: ƒ.Node = new ƒ.Node("transform");
+    transform.addComponent(new ƒ.ComponentTransform());
+    transform.mtxLocal.translation = pos;
+
+    for (let x: number = 0; x < size; x++){
+      for(let y: number = 0; y < size; y++){
+        for(let z: number = 0; z < size; z++){
+          cubes[index] = generate(index, new ƒ.Vector3(x*displacement, y*displacement, z*displacement));
+          index++;
+        }
+      }
+    }
+  }
+}
+
+function generate(i: number, pos: ƒ.Vector3): ƒ.Node {
+  throw new Error("Function not implemented.");
 }
